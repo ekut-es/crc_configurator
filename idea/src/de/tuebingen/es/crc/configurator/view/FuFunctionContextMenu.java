09a17c51e1f68899581e0c3fc048d9aaa8a13fa4
Konstantin Lübeck <konze@konze.org>
2016-09-01 15:09:54 +0200

    Refactored FuFunctionContextMenu.java

--- idea/src/de/tuebingen/es/crc/configurator/view/FuFunctionContextMenu.java
+++ idea/src/de/tuebingen/es/crc/configurator/view/FuFunctionContextMenu.java
@@ -6,6 +6,7 @@ import javafx.scene.control.ContextMenu;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by Konstantin (Konze) Lübeck on 27/08/16.
@@ -22,12 +23,12 @@ public class FuFunctionContextMenu extends ContextMenu {
 
         LinkedHashMap<String, Boolean> fuFunctions = fu.getFunctions();
 
-        for(Map.Entry<String, Boolean> entry: fuFunctions.entrySet())
-            if (entry.getValue()) if (entry.getKey() != "compare" && entry.getKey() != "multiplex") {
+        fuFunctions.entrySet().stream().filter(Map.Entry::getValue).forEachOrdered(entry -> {
+            if (!Objects.equals(entry.getKey(), "compare") && !Objects.equals(entry.getKey(), "multiplex")) {
 
                 this.addMenuItem(activeFuFunction, entry.getKey());
 
-            } else if (entry.getKey() == "compare") {
+            } else if (Objects.equals(entry.getKey(), "compare")) {
 
                 this.addMenuItem(activeFuFunction, "compare_eq");
                 this.addMenuItem(activeFuFunction, "compare_neq");
@@ -36,11 +37,12 @@ public class FuFunctionContextMenu extends ContextMenu {
                 this.addMenuItem(activeFuFunction, "compare_leq");
                 this.addMenuItem(activeFuFunction, "compare_geq");
 
-            } else if (entry.getKey() == "multiplex") {
+            } else if (Objects.equals(entry.getKey(), "multiplex")) {
 
                 this.addMenuItem(activeFuFunction, "mux_0");
                 this.addMenuItem(activeFuFunction, "mux_1");
             }
+        });
     }
 
     private void addMenuItem(PE.FUFunction activeFuFunction, String fuFunction) {
